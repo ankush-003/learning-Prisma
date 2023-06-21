37edfa1e3c8804e2e8366b372299dc8cb2acf352
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-06-21 05:51:18 +0530

    learning create operations

--- script.ts
+++ script.ts
@@ -1,9 +1,27 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient(({ log: ["query", "info", "warn"] }))
+const prisma = new PrismaClient({log: ["query"]})
 
 async function main() {
     // const user = await prisma.user.create({ data: { name: "Alice" } });
-    const user = await prisma.user.findMany();
+    // const user = await prisma.user.deleteMany();
+    // here we cannot use name as its not unique
+    // const user = await prisma.user.findUnique({
+    //     where:{
+    //         age_name:{
+    //             age:18,
+    //             name:"Ankush"
+    //         }
+    //     },include:{
+    //         userPreference:true
+    //     }
+    // })
+    const user = await prisma.user.findMany({
+        where:{
+            age: 19
+        },
+    })
+
     console.log(user);
 }
 
@@ -13,4 +31,52 @@ main()
     })
     .finally(async () => {
         await prisma.$disconnect();
+        console.log("Disconnected");
     });
+
+// async function createUser() {
+//     const user = await prisma.user.create({
+//        data: {
+//          name: "Ankush",
+//          email: "anshhv2003@gmail.com",
+//          age: 18,   
+//          userPreference: {
+//             create: {
+//                 emailUpdates: true,
+//             }
+//          }
+//        }, 
+//        include: {
+//               userPreference: true
+//        }
+//     })
+//     console.log(user);
+// }
+
+
+async function createUser() {
+    const users = await prisma.user.createMany({
+        data:[
+            {
+                name: "Ankush",
+                email: "ansh@gmail.com",
+                age: 19
+            },
+            {
+                name: "Ankush2",
+                email: "anshh@gmail.com",
+                age: 19
+            }
+        ]
+    })
+    console.log(users);
+}
+
+
+// createUser()
+//     .catch((e) => {
+//         console.error(e.message)
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect();
+//     })
